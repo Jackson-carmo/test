d029e936f787f61ba98f76cbbb7438425eb94c7a
Jackson-carmo <jacksoncruciol157@gmail.com>
2022-12-12 21:58:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route exatc path='/'/>
+    </Routes>
+    </Router>
+      
+      
+    </>
+  );
+}
+
+export default App;
